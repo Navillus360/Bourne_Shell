9fa521e7ff11665db356b87662cc20fb809f7df9
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2024-06-13 18:14:36 +0100

    Update FileChecker.sh
    This updated version of file checker allows for the user to check if a file exists in a directory/folder of their choosing.

--- FileChecker.sh
+++ FileChecker.sh
@@ -1,7 +1,19 @@
-for fileNames in *.sh; do
-if [[fileNames == "FileChecker.sh"]];
-then 
-echo "Got the origin file!"
-fi
-echo "$fileNames"
-done
+Prompt () {
+ fileName="USER INPUT"
+ dirLocation=/"USER INPUT"
+ read -p "What file would you like to find? (include file type)" fileName
+ echo #
+ read -p "Choose a target directory (Note! only target directorys from the desktop or the children folder this script is executed from!)" dirLocation
+ dirLocationConvert=$dirLocation/$fileName
+ FileCheck "$dirLocationConvert"
+}
+
+FileCheck () {
+  for fileNames in "$dirLocation"/*; do
+  if [[ $fileNames == $dirLocationConvert ]]; then
+   echo "File is in the target Directory!"
+  fi
+  done
+}
+
+Prompt
